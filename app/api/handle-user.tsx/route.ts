1bc51fbf8e96cd6faef8e392d2443f213d837131
Abdullah-1904 <98538123+Abdullah-1904@users.noreply.github.com>
2024-04-29 02:47:39 +0500

    update: clerk webhook handler

--- app/api/handle-user.tsx/route.ts
+++ app/api/handle-user.tsx/route.ts
@@ -1,51 +1,57 @@
 import type { WebhookEvent } from "@clerk/nextjs/server";
 import { supabaseServiceRole } from "../../../lib/supabase/supabaseClient";
 
-const handler = async (req) => {
+const handler = async (req, res) => {
   const evt = req.body.evt as WebhookEvent;
-  switch (evt.type) {
-    case "user.created":
-      // UserJSON.first_name is a string
-      const firstName = evt.data.first_name;
-      // UserJSON.last_name is a string
-      const lastName = evt.data.last_name;
-      // UserJSON.email_addresses is an array of EmailAddressJSON
-      const emails = evt.data.email_addresses;
+  let responseMessage = "Success";
+  console.log("user", evt.data);
+  try {
+    switch (evt.type) {
+      case "user.created":
+        const firstName = evt.data.first_name;
+        const lastName = evt.data.last_name;
+        const emails = evt.data.email_addresses;
+        const user_id = evt?.data?.id;
 
-      const user_id = evt?.data?.id;
+        const { data, error } = await supabaseServiceRole.from("user").insert([
+          {
+            user_id: user_id,
+            email: emails[0].email_address,
+          },
+        ]);
+        if (error) throw error;
+        break;
 
-      const { data, error } = await supabaseServiceRole.from("user").insert([
-        {
-          user_id: user_id,
-          email: emails[0].email_address,
-        },
-      ]);
-      break;
+      case "user.updated":
+        const updatedUser_id = evt?.data?.id;
+        const updatedEmails = evt.data.email_addresses;
 
-    case "user.updated":
-      const updatedUser_id = evt?.data?.id;
-      const updatedEmails = evt.data.email_addresses;
+        const { data: updatedData, error: updateError } =
+          await supabaseServiceRole
+            .from("user")
+            .update({
+              email: updatedEmails[0].email_address,
+            })
+            .match({
+              user_id: updatedUser_id,
+            });
+        if (updateError) throw updateError;
+        break;
 
-      const { data: updatedData, error: updateError } =
-        await supabaseServiceRole
-          .from("user")
-          .update({
-            email: updatedEmails[0].email_address,
-          })
-          .match({
-            user_id: updatedUser_id,
+      case "user.deleted":
+        const deletedUser_id = evt?.data?.id;
+
+        const { data: deletedData, error: deleteError } =
+          await supabaseServiceRole.from("user").delete().match({
+            user_id: deletedUser_id,
           });
-      break;
-
-    case "user.deleted":
-      const deletedUser_id = evt?.data?.id;
-
-      const { data: deletedData, error: deleteError } =
-        await supabaseServiceRole.from("user").delete().match({
-          user_id: deletedUser_id,
-        });
-      break;
+        if (deleteError) throw deleteError;
+        break;
+    }
+  } catch (err) {
+    responseMessage = `Failed: ${err.message}`;
+    console.error(responseMessage);
   }
 
-  return 
+  res.status(200).send(responseMessage);
 };
