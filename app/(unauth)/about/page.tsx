b409701d6ecf6a42da347a47a2cd435f6cf7812d
Abdullah-1904 <98538123+Abdullah-1904@users.noreply.github.com>
2024-05-10 05:44:57 +0500

    updating UI

--- app/(unauth)/about/page.tsx
+++ app/(unauth)/about/page.tsx
@@ -10,45 +10,53 @@ export default function About() {
     redirect("/dashboard");
   }
   return (
-    <div className="min-h-screen">
-      <h1 className="text-6xl font-bold mt-8">About</h1>
-
-      <div className="flex flex-col gap-4 p-4 bg-white shadow-md  rounded-md mt-4 ">
-        <p className="text-gray-500">
-          Welcome to our Car Rental Service, where we believe in the{" "}
-          <b>freedom</b> of the open road!
-        </p>
-        <p className="text-gray-500">
-          We are a trusted and reliable car rental platform that connects car
-          owners with potential renters. Our mission is to provide an easy,
-          secure, and efficient car rental experience for both parties involved.
-        </p>
-        <p className="text-gray-500">
-          For Car Renters: We offer a wide range of vehicles to suit your
-          preferences and needs. Whether you&apos;re planning a road trip, need
-          a vehicle for business, or just want to try out a different car model,
-          we&apos;ve got you covered. Our user-friendly platform allows you to
-          find and book your perfect ride with just a few clicks.
-        </p>
-        <p className="text-gray-500">
-          For Car Owners: We provide a platform for you to monetize your idle
-          car. List your car on our platform and start earning today. We handle
-          all the logistics, from connecting you with reliable renters to
-          ensuring secure payments.
-        </p>
-        <p className="text-gray-500">
-          Secure and Easy Payments: We prioritize your safety and security. Our
-          online payment process is smooth, secure, and transparent.
-        </p>
-        <p className="text-gray-500">
-          Customer Support: We value our customers and strive to provide
-          exceptional customer service. Our dedicated team is available 24/7 to
-          assist you with any queries or issues.
-        </p>
-        <p className="text-gray-500">
-          Join us today and experience the convenience, reliability, and freedom
-          that our Car Rental Service offers!
-        </p>
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <h1 className="text-4xl font-bold text-center mb-4 cursor-pointer">
+            About
+          </h1>
+          <div className="divide-y divide-gray-200">
+            <p className="text-gray-500">
+              Welcome to our Car Rental Service, where we believe in the{" "}
+              <b className="text-blue-500">freedom</b> of the open road!
+            </p>
+            <p className="text-gray-500">
+              We are a trusted and reliable car rental platform that connects
+              car owners with potential renters. Our mission is to provide an
+              easy, secure, and efficient car rental experience for both parties
+              involved.
+            </p>
+            <p className="text-gray-500">
+              For Car Renters: We offer a wide range of vehicles to suit your
+              preferences and needs. Whether you&apos;re planning a road trip,
+              need a vehicle for business, or just want to try out a different
+              car model, we&apos;ve got you covered. Our user-friendly platform
+              allows you to find and book your perfect ride with just a few
+              clicks.
+            </p>
+            <p className="text-gray-500">
+              For Car Owners: We provide a platform for you to monetize your
+              idle car. List your car on our platform and start earning today.
+              We handle all the logistics, from connecting you with reliable
+              renters to ensuring secure payments.
+            </p>
+            <p className="text-gray-500">
+              Secure and Easy Payments: We prioritize your safety and security.
+              Our online payment process is smooth, secure, and transparent.
+            </p>
+            <p className="text-gray-500">
+              Customer Support: We value our customers and strive to provide
+              exceptional customer service. Our dedicated team is available 24/7
+              to assist you with any queries or issues.
+            </p>
+            <p className="text-gray-500">
+              Join us today and experience the convenience, reliability, and
+              freedom that our Car Rental Service offers!
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- app/(unauth)/howtobook/page.tsx
+++ app/(unauth)/howtobook/page.tsx
@@ -12,7 +12,7 @@ const Page = () => {
   }
   return (
     <>
-      <div className="flex flex-col items-center justify-center space-y-4 p-4">
+      {/* <div className="flex flex-col items-center justify-center space-y-4 p-4">
         <h2 className="text-xl font-bold">
           Below is a step-by-step guide on how to post your car
         </h2>
@@ -73,6 +73,92 @@ const Page = () => {
           If you encounter any issues, please contact our support team for
           assistance.
         </p>
+      </div>
+    </> */}
+
+      <div className="min-h-screen bg-gradient-to-br from-blue-200 via-indigo-200 to-purple-300 py-10 flex flex-col justify-center sm:py-12">
+        <div className="relative py-5 sm:max-w-3xl sm:mx-auto">
+          <div className="absolute inset-0 bg-gradient-to-r from-purple-400 to-pink-500 shadow-lg transform skew-y-0 rotate-3 rounded-3xl"></div>
+          <div className="relative px-6 py-16 bg-white shadow-lg rounded-3xl p-20">
+            <h1 className="text-5xl font-bold text-center mb-10 cursor-pointer">
+              How to Post Your Car
+            </h1>
+            <div className="divide-y divide-gray-200">
+              <div className="text-gray-600 font-medium">
+                <ol className="list-decimal space-y-4 pl-5">
+                  <li>
+                    <strong>Sign in:</strong> Access our website and click on
+                    the 'Sign in' button at the top right corner to log in with
+                    your credentials. If you're new, click 'Sign Up' to create
+                    an account.
+                    <div className="mt-2 flex justify-start">
+                      <Image
+                        src="/signinbutton.png"
+                        alt="sign in button"
+                        width={180}
+                        height={120}
+                      />
+                    </div>
+                  </li>
+                  <li>
+                    <strong>Navigate to My Postings:</strong> Once logged in,
+                    find the 'My Postings' section in the dashboard to view and
+                    manage your postings.
+                    <div className="mt-2 flex justify-start">
+                      <Image
+                        src="/dashboard.png"
+                        alt="dashboard"
+                        width={180}
+                        height={120}
+                      />
+                    </div>
+                  </li>
+                  <li>
+                    <strong>Create a New Posting:</strong> In 'My Postings',
+                    click the 'New Posting' button to start listing your car.
+                    <div className="mt-2 flex justify-start">
+                      <Image
+                        src="/newposting.png"
+                        alt="new post"
+                        width={180}
+                        height={120}
+                      />
+                    </div>
+                  </li>
+                  <li>
+                    <strong>Add Car Information:</strong> Complete the form with
+                    your car's details, such as make, model, year, and rental
+                    price.
+                    <div className="mt-2 flex justify-start">
+                      <Image
+                        src="/form.png"
+                        alt="new post"
+                        width={990}
+                        height={220}
+                      />
+                    </div>
+                  </li>
+                  <li>
+                    <strong>Submit Your Posting:</strong> Review the information
+                    and hit 'Create' to add your car to the rental listings.
+                    <div className="mt-2 flex justify-start">
+                      <Image
+                        src="/create.png"
+                        alt="new post"
+                        width={110}
+                        height={120}
+                      />
+                    </div>
+                  </li>
+                </ol>
+              </div>
+              <p className="pt-6 text-gray-600 text-center">
+                Carefully follow these steps to list your car for rental. For
+                any assistance, our support team is here to help 24/7.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
